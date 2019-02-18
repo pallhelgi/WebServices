50b7f39853aa63f70a9a9f6e201f6a0a62db7a8d
pallhelgi <pallhelgi@gmail.com>
2019-02-18 20:57:38 +0000

    Project done as per project description

--- DotNetCore/tinysoldiers/Controllers/ModelsController.cs
+++ DotNetCore/tinysoldiers/Controllers/ModelsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Http.Extensions;
 using System;
 using System.Linq;
 using tinysoldiers.Models;
@@ -26,6 +27,14 @@ namespace tinysoldiers.Controllers
                 return BadRequest();
             }
 
+            // AcceptLanguage is default en-US
+            var acceptLanguage = "en-US";
+            var l = HttpContext.Request.Headers["Accept-Language"].ToString();
+            if (l != "")
+            {
+                acceptLanguage = l;
+            }
+
             // Initiating an empty list of ModelDTO that will be included in the envelope
             List<ModelDTO> modelDTOList = new List<ModelDTO>();
 
@@ -33,7 +42,13 @@ namespace tinysoldiers.Controllers
             for (int i = ((pageNumber - 1) * pageSize); i < pageNumber * pageSize; i++)
             {
                 // ToLightWeight changed Model to ModelDTO
-                modelDTOList.Add(DataContext.Models.ToLightWeight()[i]);
+                modelDTOList.Add(DataContext.Models.ToLightWeight(acceptLanguage)[i]);
+            }
+
+            // Adding HATEOS reference to each models self
+            foreach (ModelDTO m in modelDTOList)
+            {
+                m.Links.AddReference("self", HttpContext.Request.GetDisplayUrl() + "/" + m.Id);
             }
 
             Envelope<ModelDTO> env = new Envelope<ModelDTO>();
@@ -48,12 +63,24 @@ namespace tinysoldiers.Controllers
         [HttpGet("{Id:int}")]
         public IActionResult GetModelById(int Id)
         {
-            List<ModelDetailsDTO> model = DataContext.Models.FindAll(x => x.Id == Id).ToDetails();
-            if (model.Count == 0)
+            // AcceptLanguage is default en-US
+            var acceptLanguage = "en-US";
+            var l = HttpContext.Request.Headers["Accept-Language"].ToString();
+            if (l != "")
+            {
+                acceptLanguage = l;
+            }
+
+            ModelDetailsDTO model = DataContext.Models.ToDetails(acceptLanguage).FirstOrDefault(x => x.Id == Id);
+            if (model == null)
             {
                 return BadRequest();
             }
-            return Ok(model[0]);
+
+            // Adding HATEOS reference to self
+            model.Links.AddReference("self", HttpContext.Request.GetDisplayUrl());
+
+            return Ok(model);
         }
     }
 }
