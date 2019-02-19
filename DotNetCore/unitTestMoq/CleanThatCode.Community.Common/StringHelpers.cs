0c8dc02b10423ca1ffb270aa7b16235dfd85ed6c
pallhelgi <pallhelgi@gmail.com>
2019-02-19 19:31:01 +0000

    initial push for unit test with Moq project

---
+++ DotNetCore/unitTestMoq/CleanThatCode.Community.Common/StringHelpers.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+
+namespace CleanThatCode.Community.Common
+{
+    // Your job is to implement this class
+    public static class StringHelpers
+    {
+        // Instead of spaces it should be separated with dots, e.g. Hello World -> Hello.World
+        public static string ToDotSeparatedString(this string str)
+        {
+            return "";
+        }
+        
+        // All words in the string should be capitalized, e.g. teenage mutant ninja turtles -> Teenage Mutant Ninja Turtles
+        public static string CapitalizeAllWords(this string str)
+        {
+            return "";
+        }
+
+        // The words should be reversed in the string, e.g. Hi Ho Silver Away! -> Away! Silver Ho Hi
+        public static string ReverseWords(this string str)
+        {
+            return "";
+        }
+    }
+}
