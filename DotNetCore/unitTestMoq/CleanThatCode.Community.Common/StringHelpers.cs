bb8d97a6c5b87fa63b344995a4692446e0333be9
pallhelgi <pallhelgi@gmail.com>
2019-02-20 14:28:23 +0000

    part one done

--- DotNetCore/unitTestMoq/CleanThatCode.Community.Common/StringHelpers.cs
+++ DotNetCore/unitTestMoq/CleanThatCode.Community.Common/StringHelpers.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CleanThatCode.Community.Common
@@ -7,20 +9,29 @@ namespace CleanThatCode.Community.Common
     {
         // Instead of spaces it should be separated with dots, e.g. Hello World -> Hello.World
         public static string ToDotSeparatedString(this string str)
-        {
-            return "";
+        {   
+            str = str.Replace(" ", ".");
+            return str;
         }
         
         // All words in the string should be capitalized, e.g. teenage mutant ninja turtles -> Teenage Mutant Ninja Turtles
         public static string CapitalizeAllWords(this string str)
         {
-            return "";
+            var split = str.Split(' ');
+            List<String> li = new List<String>();
+
+            foreach(string s in split)
+            {
+                li.Add(char.ToUpper(s[0]) + s.Substring(1));
+            }
+            str = String.Join(" ", li);
+            return str;
         }
 
         // The words should be reversed in the string, e.g. Hi Ho Silver Away! -> Away! Silver Ho Hi
         public static string ReverseWords(this string str)
         {
-            return "";
+            return String.Join(" ", str.Split(' ').Reverse());
         }
     }
 }
